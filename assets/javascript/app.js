c7ba3294bb5ae0ebb82abd032318e0a0169272fa
Kelsi Summers <kelsilsummers@gmail.com>
2018-01-14 15:47:27 -0700

    auth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -144,3 +144,22 @@ database.ref().on('child_added', function(childSnapshot){
 }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
