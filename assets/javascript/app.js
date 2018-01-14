e46b2b36ea31f2fd576e3ad4b22ac7f48af460cd
Kelsi Summers <kelsilsummers@gmail.com>
2018-01-14 15:34:54 -0700

    auth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -150,19 +150,21 @@ var provider = new firebase.auth.GoogleAuthProvider();
 
 firebase.auth().signInWithRedirect(provider);
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+
+
+  var ref = new Firebase("https://train-schedule-dcf8a.firebaseio.com/");
+
+  
+ref.onAuth(function(authData) {
+  if (authData !== null) {
+    console.log("Authenticated successfully with payload:", authData);
+  } else {
+    // Try to authenticate with Google via OAuth redirection
+    firebase.auth().signInWithRedirect(provider);
+    ref.authWithOAuthRedirect("google", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      }
+    });
+  }
+})
