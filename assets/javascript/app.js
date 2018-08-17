a8606762b55aeadb89ab0834dd409d17e7798cbe
Kelsi Summers <kelsilsummers@gmail.com>
2018-08-17 15:02:25 -0600

    adding responsive queries and removing console logs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,13 +21,13 @@ $(('#submit-button')).on('click', function (event){
 
     // Stores Inputs as Variables
     var trainName= $("#train-name").val().trim();
-    console.log('Train Name:', trainName);
+    // console.log('Train Name:', trainName);
     var destination = $("#destination").val().trim();
-    console.log('Destination:', destination);
+    // console.log('Destination:', destination);
     var firstTrain = $("#first-train").val().trim();
-    console.log('First Train:', firstTrain)
+    // console.log('First Train:', firstTrain)
     var frequency = $("#frequency").val().trim();
-    console.log('Frequency', frequency)
+    // console.log('Frequency', frequency)
    
     // Prevents Submit If Fields Are Empty
     if (trainName==="" || destination ==="" || frequency ==="") {
@@ -58,11 +58,11 @@ $(('#submit-button')).on('click', function (event){
 database.ref().on('child_added', function(childSnapshot){
 
     // Log everything that's coming out of snapshot
-    console.log('Key:', childSnapshot.key)
-    console.log('Train Name:', childSnapshot.val().trainName);
-    console.log('Destination:', childSnapshot.val().destination);
-    console.log('First Train:', childSnapshot.val().firstTrain);
-    console.log('Frequency:', childSnapshot.val().frequency);
+    // console.log('Key:', childSnapshot.key)
+    // console.log('Train Name:', childSnapshot.val().trainName);
+    // console.log('Destination:', childSnapshot.val().destination);
+    // console.log('First Train:', childSnapshot.val().firstTrain);
+    // console.log('Frequency:', childSnapshot.val().frequency);
 
     // Store everything into a variable
     var trainKey = childSnapshot.key;
@@ -76,7 +76,7 @@ database.ref().on('child_added', function(childSnapshot){
 
     // Adds Frequency Minutes to Calculate Next Arrival
     var nextArrival = moment(firstTrainMoment, 'hh:mm A').add(frequencyChild, 'm').format('hh:mm A');
-    console.log('Next Arrival:', nextArrival);
+    // console.log('Next Arrival:', nextArrival);
     
     // Difference Between Present Moment and Next Arrival in Minutes
     var minutesAway = moment(nextArrival,'hh:mm A').diff(moment(), 'minutes');
@@ -84,9 +84,9 @@ database.ref().on('child_added', function(childSnapshot){
     // If minutesAway are < 0, Add 1440 Minutes (24 hours)
     if (minutesAway < 0) {
         minutesAway += 1440;
-        console.log('Minutes Away:', minutesAway);
+        // console.log('Minutes Away:', minutesAway);
     } else {
-        console.log('Minutes Away:', minutesAway);
+        // console.log('Minutes Away:', minutesAway);
     }
  
     // Write Train Information from Firebase to HTML
@@ -95,8 +95,8 @@ database.ref().on('child_added', function(childSnapshot){
     '<div class="destination col m2" contenteditable="true">' + childSnapshot.val().destination + '</div>' +
     '<div class="frequency col m2">' + childSnapshot.val().frequency + '</div>' +
     '<div class="next-arrival col m2 " contenteditable="true">' + nextArrival + '</div>' +
-    '<div class="minutes-away col m1 ">' + minutesAway + '</div>' 
-    + '<div class="col m3"><a class="update waves-effect waves-light btn">Update</a><a class="remove waves-effect waves-light btn">Remove</a></div></div>');
+    '<div class="minutes-away col m2 ">' + minutesAway + '</div>' 
+    + '<div class="col m2 button-column"><a class="update waves-effect waves-light btn">Update</a><a class="remove waves-effect waves-light btn">Remove</a></div></div>');
     
     // Handle the errors
 }, function(errorObject) {
@@ -109,17 +109,17 @@ database.ref().on('child_added', function(childSnapshot){
 $(document).on('click', '.update', function(update){
     var row = $(this).closest('.row');
     var rowId = row.attr('id');
-    console.log('rowId:', rowId);
+    // console.log('rowId:', rowId);
     
     // Save New Text as Variables
     var trainName = $('#'+rowId+' .train-name').text();
-    console.log('Update:', trainName);
+    // console.log('Update:', trainName);
     var destination = $('#'+rowId+' .destination').text();
-    console.log('Update:', destination);
+    // console.log('Update:', destination);
     var nextTrain = $('#'+rowId+' .next-arrival').text();
-    console.log('Update:', nextTrain);
+    // console.log('Update:', nextTrain);
     var frequency = $('#'+rowId+' .frequency').text();
-    console.log('Update:', frequency);
+    // console.log('Update:', frequency);
 
     // Overwrite Firebase with Changed Variables
     database.ref().child(rowId).set({ 
@@ -134,7 +134,7 @@ $(document).on('click', '.update', function(update){
     
     // Adds Frequency Minutes to Calculate Next Arrival
     var nextArrival = moment(nextTrainMoment).add(frequency, 'm').format('hh:mm A');
-    console.log('Next Arrival:', nextArrival);
+    // console.log('Next Arrival:', nextArrival);
 
     // Difference Between Present Moment and Next Arrival in Minutes
     var minutesAway = moment(nextArrival,'hh:mm A').diff(moment(), 'minutes');
@@ -142,14 +142,14 @@ $(document).on('click', '.update', function(update){
     // If minutesAway are < 0, Add 1440 Minutes (24 hours)
     if (minutesAway < 0) {
         minutesAway += 1440;
-        console.log('Minutes Away:', minutesAway);
+        // console.log('Minutes Away:', minutesAway);
     } else {
-        console.log('Minutes Away:', minutesAway);
+        // console.log('Minutes Away:', minutesAway);
     };
 
     // Update Minutes Away in HTML
     $('#'+rowId+' .minutes-away').text(minutesAway);
-    console.log('Update:', minutesAway);
+    // console.log('Update:', minutesAway);
 });
 
 
@@ -158,7 +158,7 @@ $(document).on('click', '.update', function(update){
 $(document).on('click','.remove', function(remove){
     var row = $(this).closest('.row');
     var rowId = row.attr('id');
-    console.log('rowId:', rowId);
+    // console.log('rowId:', rowId);
 
     // Deletes Row from HTML
     row.remove();
